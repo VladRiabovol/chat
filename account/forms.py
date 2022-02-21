9a744ef922d89cafd66fb319993bc47001f26bf4
VladRiabovol <82464669+VladRiabovol@users.noreply.github.com>
2022-02-21 19:01:27 +0200

    create login & logout

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 from account.models import Account
 
@@ -27,3 +27,25 @@ class RegistrationForm(UserCreationForm):
         except Exception as e:
             return username
         raise forms.ValidationError(f'Username {username} is already in use')
+
+
+class AccountAuthenticationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ('email', 'password')
+
+    def save(self):
+        email = self.cleaned_data['email']
+        password = self.cleaned_data['password']
+        user = authenticate(email=email, password=password)
+        if user:
+            login(request, user)
+
+    def clean(self):
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError('Invalid email or password')
